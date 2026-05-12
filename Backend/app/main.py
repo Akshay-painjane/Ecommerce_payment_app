cc0197881596f34e41dcfba7d875c808ac15ab6c
monika-axiorag <monika.ch@axioraglobalsolutions.com>
2026-05-12 13:24:14 +0530

    Added ecommerce frontend UI

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -1,12 +1,8 @@
 from fastapi import FastAPI
-from Backend.app.auth import router as auth_router
-from Backend.app.logs import logger
 
 app = FastAPI(title="Ecommerce Payment App")
 
-app.include_router(auth_router)
 
 @app.get("/")
 def home():
-    logger.info("Home API called")
     return {"message": "Ecommerce Payment App API is running"}
