0b4437c163e7fbf8fb13c27d213e9bee60b56a4b
deepthi-axiora <Deepthi.u@axioraglobalsolutions.com>
2026-05-13 09:55:45 +0530

    added product and category realtionship

--- Backend/app/models/category.py
+++ Backend/app/models/category.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 
 from app.database import Base
 
@@ -10,3 +11,6 @@ class Category(Base):
     id = Column(Integer, primary_key=True, index=True)
 
     name = Column(String, unique=True, nullable=False)
+
+    # relationship with Product
+    products = relationship("Product", back_populates="category")
